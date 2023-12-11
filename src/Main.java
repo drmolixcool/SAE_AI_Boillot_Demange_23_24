778c73487f70cdab7fc08446a534eeaca4e84b28
Thomas <thomas.boillot@etu.univ-lorraine.fr>
2023-12-11 15:08:08 +0100

    Fin d'implémentation de MLP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -26,32 +29,44 @@ public class Main {
         return array;
     }
     public static void main(String[] args) throws Exception {
-        int[] layers = {2,2,1};
+        int[] layers = {2,10,1};
         double learningRate = Double.parseDouble(args[0]);
         String transferFunctionName = args[1];
         TransferFunction tf = null;
-        if (Objects.equals(transferFunctionName, "sigmoid")) tf = new SigmoidFunction();
-        else if (Objects.equals(transferFunctionName, "tanh")) tf = new TanHFunction();
-        else throw new Exception("Mauvais argument 2 : doit être sigmoide ou tanh");
+        if (Objects.equals(transferFunctionName, "sigmoid"))
+            tf = new SigmoidFunction();
+        else if (Objects.equals(transferFunctionName, "tanh")){
+            tf = new TanHFunction();
+        }
+        else throw new Exception("Mauvais argument 2 : doit être sigmoid ou tanh");
 
         MLP mlp = new MLP(layers, learningRate, tf);
 
         double[][] and = {{0,0,0}, {0,1,0}, {1,0,0}, {1,1,1}};
         double[][] or = {{0,0,0}, {0,1,1}, {1,0,1}, {1,1,1}};
         double[][] xor = {{0,0,0}, {0,1,1}, {1,0,1}, {1,1,0}};
+        List<Boolean> appris = new ArrayList<>(List.of(false, false, false, false));
+        double[][] tablePrise = transformInput(xor, (FalseValue) tf);
 
-        int nbIterations = 1000;
+        int nbIterations = 5000;
         for (int i=1; i<=nbIterations;i++){
             System.out.println("Itération : " + i);
-            for (double[] sousTable : xor) {
-                double error = mlp.backPropagate(new double[]{sousTable[0], sousTable[1]}, new double[]{sousTable[2]});
-                System.out.println(error);
-            }
+
+            int randomIndex = getRandomIndexWhichFalse(appris);
+
+            double[] randomEntry = tablePrise[randomIndex];
+
+            double error = mlp.backPropagate(new double[]{randomEntry[0], randomEntry[1]}, new double[]{randomEntry[2]});
+            if (error<0.05) appris.set(randomIndex, true);
+            System.out.println(error);
+
+            if (!appris.contains(false)) break;
+
         }
 
         System.out.println("***************************");
 
-        for (double[] sousTable : xor){
+        for (double[] sousTable : tablePrise){
             double[] res = mlp.execute(new double[]{sousTable[0], sousTable[1]});
             System.out.println(Arrays.toString(res));
         }
