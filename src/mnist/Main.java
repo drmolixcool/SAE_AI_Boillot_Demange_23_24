aa324ca79839b5cb1d6e390be0c3b63990eef2c0
Hugo Demange <hugo.demange8@etu.univ-lorraine.fr>
2023-12-11 10:47:26 +0100

    clean code

--- src/mnist/Main.java
+++ src/mnist/Main.java
@@ -1,12 +1,8 @@
 package mnist;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.*;
-import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) throws IOException {
